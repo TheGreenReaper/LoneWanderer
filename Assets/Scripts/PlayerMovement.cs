1500d1f1c3bc8af47ddd657bad0d18de608cf1f0
TheGreenReaper <daniel.lalov@gmail.com>
2018-09-12 23:12:23 +0100

    First Commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,40 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using PathFind;
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using PathFind;
 
-public class PlayerMovement : MonoBehaviour {
+    public class PlayerMovement : MonoBehaviour {
     public float speed;
+    private Vector2 nextPosition;
+    private Vector2 currentPosition;
+    private List<Point> path;
+    private GameObject tile;
+    private Point _to;
+    private Point _from;
+
+    private void Start()
+    {
+        nextPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+    }
 
     void Update()
     {
+        
+        currentPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
         if (Input.GetMouseButtonDown(1))
         {
-            StopAllCoroutines();
-            GameObject tile;
-            Point _to;
-            Point _from;
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            FindPath();
+        }
+        if (currentPosition != nextPosition)
+        {
+            transform.position = Vector2.Lerp(currentPosition, nextPosition, speed);
+        }
+        if (path != null && currentPosition == nextPosition)
+        {
+            SetNextNode();
+        }
+            
+        //path.RemoveAt(0);
+        //}
+    }
 
-            if (hit.collider != null)
+    void FindPath()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+        {
+            tile = hit.collider.gameObject;
+            if (tile.tag == "canWalk")
             {
-                tile = hit.collider.gameObject;
-                if (tile.tag == "canWalk")
-                {
-                    _to = new Point(Mathf.RoundToInt(tile.transform.position.x), Mathf.RoundToInt(tile.transform.position.y));
-                    _from = new Point(Mathf.RoundToInt(gameObject.transform.position.x), Mathf.RoundToInt(gameObject.transform.position.y));
-                    List<Point> path = Pathfinding.FindPath(gameObject.GetComponent<GridController>().GetGrid(), _from, _to);
-                    StartCoroutine(MoveUser(path));
-                }
+                _to = new Point(Mathf.RoundToInt(tile.transform.position.x), Mathf.RoundToInt(tile.transform.position.y));
+                _from = new Point(Mathf.RoundToInt(gameObject.transform.position.x), Mathf.RoundToInt(gameObject.transform.position.y));
+                path = Pathfinding.FindPath(gameObject.GetComponent<GridController>().GetGrid(), _from, _to);
             }
         }
     }
-    IEnumerator MoveUser(List<Point> path)
-    { 
-        foreach (Point point in path)
+    void SetNextNode()
+    {
+        int length = 0;
+        foreach (Point node in path)
         {
-            transform.position = new Vector2(point.x, point.y);
-            yield return new WaitForSeconds(speed);
+            length++;
+        }
+        if (length > 0)
+        {
+            nextPosition = new Vector2(path[0].x, path[0].y);
+            path.RemoveAt(0);
+            Debug.Log("Next Position: " + nextPosition + " Remaining: " + length);
         }
     }
-}
+    }

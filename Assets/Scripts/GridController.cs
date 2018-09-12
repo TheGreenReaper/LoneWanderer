4b4cb538725be15d6f2aa1ae3dcb406c99590a87
TheGreenReaper <daniel.lalov@gmail.com>
2018-09-12 21:56:03 +0100

    Initial commit

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathFind;
+
+public class GridController : MonoBehaviour {
+    public int width;
+    public int height;
+    GameObject[] canWalk;
+    GameObject[] cannotWalk;
+
+    void Start () {
+        GetGrid();
+    }
+
+    public PathFind.Grid GetGrid()
+    {
+        bool[,] tilesmap = new bool[width, height];
+        canWalk = GameObject.FindGameObjectsWithTag("canWalk");
+        cannotWalk = GameObject.FindGameObjectsWithTag("cannotWalk");
+        foreach (GameObject tile in canWalk){
+            tilesmap[Mathf.RoundToInt(tile.transform.position.x), Mathf.RoundToInt(tile.transform.position.y)] = true;
+        }
+        foreach (GameObject tile in cannotWalk){
+            tilesmap[Mathf.RoundToInt(tile.transform.position.x), Mathf.RoundToInt(tile.transform.position.y)] = false;
+        }
+        PathFind.Grid grid = new PathFind.Grid(width, height, tilesmap);
+        return grid;
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathFind;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            StopAllCoroutines();
+            GameObject tile;
+            Point _to;
+            Point _from;
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                tile = hit.collider.gameObject;
+                if (tile.tag == "canWalk")
+                {
+                    _to = new Point(Mathf.RoundToInt(tile.transform.position.x), Mathf.RoundToInt(tile.transform.position.y));
+                    _from = new Point(Mathf.RoundToInt(gameObject.transform.position.x), Mathf.RoundToInt(gameObject.transform.position.y));
+                    List<Point> path = Pathfinding.FindPath(gameObject.GetComponent<GridController>().GetGrid(), _from, _to);
+                    StartCoroutine(MoveUser(path));
+                }
+            }
+        }
+    }
+    IEnumerator MoveUser(List<Point> path)
+    { 
+        foreach (Point point in path)
+        {
+            transform.position = new Vector2(point.x, point.y);
+            yield return new WaitForSeconds(speed);
+        }
+    }
+}
